49b1fb6c6c33ebbf484cac3fe76628925cea38be
Gian Sass <gian.sass@outlook.de>
2019-11-29 12:29:14 +0100

    Update tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,4 +2,18 @@
 
 cd ../ && make && cd tests
 
-find . -iname "run_*" -type f -not -path "./run_tests.sh" -exec sh {} \;
+for f in *.sh
+do
+  if [ "$f" != "run_tests.sh" ]; then
+    echo "Running $f"
+    sh $f
+
+    if [ ! $? -eq 0 ]; then
+      echo "$f failed"
+      exit 1
+    fi
+  fi
+done
+
+echo "Everything working fine!"
+exit 0
